2272dbb1206dbb35b4bfa875beb025d50c2c03da
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2023-04-09 22:27:10 +0200

    label axes in plot

--- plot.py
+++ plot.py
@@ -2,12 +2,14 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 
-def plot_attack():
+def plot_attack_insec():
     x=[]
     for i in range(27,35):
         x.append(i)
     y=[10.996012449264526,20.09508490562439,39.23932504653931,62.10986256599426,281.43727254867554,1043.6157057285309,1876.9959406852722,1986.2639257907867 ]
     plt.plot(x, y)
+    plt.xlabel("numner of bits of key ")
+    plt.ylabel("time for attacking in sec")
     plt.show()
 
-plot_attack()    
+plot_attack_insec()    
