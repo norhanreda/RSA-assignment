bf5edbcb37a37b298c9a2a8d39fc492d0f630e2b
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2023-04-09 22:44:42 +0200

    conversion

--- plot.py
+++ plot.py
@@ -2,18 +2,20 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 
-def plot_attack_insec():
+def plot_attack():
     x=[]
     for i in range(27,35):
         x.append(i)
     y=[10.996012449264526,20.09508490562439,39.23932504653931,62.10986256599426,281.43727254867554,1043.6157057285309,1876.9959406852722,1986.2639257907867 ]
+    y=np.asarray(y)
+    #y=y/3600
     plt.plot(x, y)
     plt.title("attacking analysis")
     plt.xlabel("numner of bits of key ")
     plt.ylabel("time for attacking in sec")
     plt.show()
 
-plot_attack_insec()   
+plot_attack()   
 
 
 def plot_encryption_insec():
@@ -27,17 +29,18 @@ def plot_encryption_insec():
     plt.ylabel("time for encryption in sec")
     plt.show()
 
-plot_encryption_insec()   
+#plot_encryption_insec()   
 
 def plot_decryption_insec():
     x=[]
     for i in range(27,35):
         x.append(i)
     y=[10.996012449264526,20.09508490562439,39.23932504653931,62.10986256599426,281.43727254867554,1043.6157057285309,1876.9959406852722,1986.2639257907867 ]
+   
     plt.plot(x, y)
     plt.title("decryption analysis")
     plt.xlabel("numner of bits of key ")
     plt.ylabel("time for decryption in sec")
     plt.show()
 
-plot_decryption_insec()   
+#plot_decryption_insec()   
